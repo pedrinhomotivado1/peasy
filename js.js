9a037d01f7d85decccf348b20365630b8fe55418
pedrinhomotivado1 <pedroginofera@gmail.com>
2022-10-26 14:08:39 -0300

    26out

--- js.js
+++ js.js
@@ -1,10 +1,15 @@
-function menu() {
+function abrirmenu() {
    let menu = document.querySelector('.menuarea')
-   if (menu.classList.contains('opened')=== true) {
-      menu.classList.remove('opened')
-      menu.classList.add('closed')      
+   if (menu.classList.contains('open')=== true) {
+      menu.classList.remove('open')
+      menu.classList.add('close')      
    } else {
-      menu.classList.remove('closed')
-      menu.classList.add('opened')
+      menu.classList.remove('close')
+      menu.classList.add('open')
    }
 }
+
+//////////////////////////////////// função do menu
+function myFunction(x) {
+   x.classList.toggle("change");
+ }

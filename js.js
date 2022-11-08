de2eb1794db9d8e49245cedde37e684e800c3c52
pedrinhomotivado1 <pedroginofera@gmail.com>
2022-11-08 18:10:45 -0300

    08 novembro

--- js.js
+++ js.js
@@ -10,6 +10,7 @@ function abrirmenu() {
 }
 //////////////////////////////////////////// inicio da area modal ////////////////////////////////////////
 
+//////////////////////////////////////////// modal box 1 ////////////////////////////////////////
 // Get the modal
 var modal0 = document.getElementById("myModal0");
 // Get the button that opens the modal
@@ -31,6 +32,8 @@ window.onclick = function(event) {
   }
 }
 
+//////////////////////////////////////////// modal box 2 ////////////////////////////////////////
+
 // Get the modal
 var modal1 = document.getElementById("myModal1");
 // Get the button that opens the modal
@@ -51,7 +54,92 @@ window.onclick = function(event) {
     modal1.style.display = "none";
   }
 }
+//////////////////////////////////////////// modal box 3 ////////////////////////////////////////
 
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn2");
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("closemodal")[2];
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+//////////////////////////////////////////// modal box 4 ////////////////////////////////////////
+// Get the modal
+var modal3 = document.getElementById("myModal3");
+// Get the button that opens the modal
+var btn3 = document.getElementById("myBtn3");
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("closemodal")[3];
+// When the user clicks on the button, open the modal
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
+
+//////////////////////////////////////////// modal box 5 ////////////////////////////////////////
+// Get the modal
+var modal4 = document.getElementById("myModal4");
+// Get the button that opens the modal
+var btn4 = document.getElementById("myBtn4");
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("closemodal")[4];
+// When the user clicks on the button, open the modal
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
+//////////////////////////////////////////// modal box 6 ////////////////////////////////////////
+// Get the modal
+var modal5 = document.getElementById("myModal5");
+// Get the button that opens the modal
+var btn5 = document.getElementById("myBtn5");
+// Get the <span> element that closes the modal
+var span5 = document.getElementsByClassName("closemodal")[5];
+// When the user clicks on the button, open the modal
+btn5.onclick = function() {
+  modal5.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+}
 //////////////////////////////////////////// fim da area modal ////////////////////////////////////////
 
 

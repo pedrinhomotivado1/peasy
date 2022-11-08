1d28d77082e2dbfc9237fb2805b5e2df2b544c42
pedrinhomotivado1 <pedroginofera@gmail.com>
2022-11-08 13:49:04 -0300

    8 novemmbro

--- js.js
+++ js.js
@@ -8,17 +8,36 @@ function abrirmenu() {
       menu.classList.add('open')
    }
 }
+//////////////////////////////////////////// inicio da area modal ////////////////////////////////////////
 
-function abrirsaibamais() {
-   let text = document.querySelector('.cardtext')
-   if (text.classList.contains('closetext')=== true) {
-      text.classList.remove('closetext')
-      text.classList.add('opentext')
-   } else {
-      text.classList.remove('opentext')
-      text.classList.add('closetext')
-   }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closemodal")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//////////////////////////////////////////// fim da area modal ////////////////////////////////////////
+
 
 
 

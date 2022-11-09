cd8b7a37033e578b1a37359944001b3e8b4b4d82
pedrinhomotivado1 <pedroginofera@gmail.com>
2022-11-09 14:53:13 -0300

    9 novembro

---
+++ jsbombar.js
@@ -0,0 +1,10 @@
+var conteinerplan = document.querySelectorAll('.areaplangeral')[0]
+var buttonsaibamaisplan = document.querySelectorAll('.buttonsaibamaisplan')[0]
+
+function abrirplan() {
+    if (conteinerplan.classList.contains('closeplan')) {
+        conteinerplan.classList.remove('closeplan')
+    } else {
+        conteinerplan.classList.add('closeplan')
+    }
+}
